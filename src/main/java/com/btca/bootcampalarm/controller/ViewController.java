2b0dc5712acd1188b82fc83d1bd357ad49dc4424
seolbotdae <seolbotdae@gmail.com>
2023-02-22 19:36:23 +0900

    Feat: index.html 로 기본 뷰 설정
    - 시작화면을 index.html 로 매핑하기 위해서 hello.html 삭제
    - ViewController 내부 메서드 수정
        - hello() 에서 indexView() 로 수정
        - index.html 을 기본으로 주기 위해 / 주소를 index 에 매핑

--- src/main/java/com/btca/bootcampalarm/controller/ViewController.java
+++ src/main/java/com/btca/bootcampalarm/controller/ViewController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "/hello.html";
+    @GetMapping("/")
+    public String indexView() {
+        return "index";
     }
 }
