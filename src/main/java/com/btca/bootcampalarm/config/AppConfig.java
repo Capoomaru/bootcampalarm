03bf15f4f7111c13761b5ec525c034524f2be679
kangchanghan <anycar8515@ynu.ac.kr>
2023-02-27 15:51:50 +0900

    Refactor : import 정리 및 변수명 변경
    - BootcampService
        - repository -> bootCampRepository
    
    - AppConfig
        - import 최적화

--- src/main/java/com/btca/bootcampalarm/config/AppConfig.java
+++ src/main/java/com/btca/bootcampalarm/config/AppConfig.java
@@ -1,9 +1,6 @@
 package com.btca.bootcampalarm.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class AppConfig {
--- src/main/java/com/btca/bootcampalarm/service/BootcampService.java
+++ src/main/java/com/btca/bootcampalarm/service/BootcampService.java
@@ -13,13 +13,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class BootcampService {
-    private final BootcampRepository repository;
+    private final BootcampRepository bootCampRepository;
 
     @Transactional(readOnly = true)
     public List<List<BootcampDto>> getBootcampList() {
         List<List<BootcampDto>> bootcampList = new ArrayList<>();
         for (BootcampType type : BootcampType.values()) {
-            List<BootcampDto> typeList = repository.findByType(type);
+            List<BootcampDto> typeList = bootCampRepository.findByType(type);
             bootcampList.add(typeList);
         }
         return bootcampList;
