adb42deba7e8ff974358917a6c8b36f1f62552d3
kangchanghan <anycar8515@ynu.ac.kr>
2023-03-01 01:22:50 +0900

    Docs : swagger parameter 설명 추가

--- src/main/java/com/btca/bootcampalarm/controller/SubscribeController.java
+++ src/main/java/com/btca/bootcampalarm/controller/SubscribeController.java
@@ -3,6 +3,7 @@ package com.btca.bootcampalarm.controller;
 import com.btca.bootcampalarm.dto.SaveRequestDto;
 import com.btca.bootcampalarm.service.UserService;
 import com.btca.bootcampalarm.util.MailUtils;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,13 +22,16 @@ public class SubscribeController {
         if(!mailUtils.mailFormatCheck(saveRequestDto.getMail()))
             throw new IllegalArgumentException("이메일 형식이 아닙니다");
 
-        userService.updateSubscribe(saveRequestDto.getMail(), saveRequestDto.getCode(), saveRequestDto.getSubscribeList());
+        userService.updateSubscribe(saveRequestDto.getMail(), saveRequestDto.getCode(),
+                saveRequestDto.getSubscribeList());
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<Long>> getSubscribeList(@RequestParam String mail, @RequestParam Integer code) {
+    public ResponseEntity<List<Long>> getSubscribeList(
+            @Parameter(name="mail", description = "abc@test.com") @RequestParam String mail,
+            @Parameter(name="code",description = "123456") @RequestParam Integer code) {
         if(!mailUtils.mailFormatCheck(mail))
             throw new IllegalArgumentException("이메일 형식이 아닙니다");
 
